f9c15d81db3fdb25fb2aaf9031f08f0798755802
hounjini <hounjini@gmail.com>
2012-08-20 11:04:51 +0900

    [SWEEP2WAKE] Forced  home key cancelation
    Forced home key cancelation to prevent screen capture event on Sense rom, and probably improve sweep2wake screen off feature on AOSP rom.

--- drivers/input/touchscreen/atmel.c
+++ drivers/input/touchscreen/atmel.c
@@ -165,6 +165,7 @@ extern void sweep2wake_setleddev(struct led_classdev * led_dev) {
 EXPORT_SYMBOL(sweep2wake_setleddev);
 
 static void sweep2wake_presspwr(struct work_struct * sweep2wake_presspwr_work) {
+	input_event(sweep2wake_pwrdev, EV_KEY, KEY_HOME, 0);
 	input_event(sweep2wake_pwrdev, EV_KEY, KEY_POWER, 1);
 	input_event(sweep2wake_pwrdev, EV_SYN, 0, 0);
 	msleep(100);
